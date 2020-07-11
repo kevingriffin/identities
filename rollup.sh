17e9c02a1acbc7d1c82352a2e501f5d5dbcef163
Kevin Griffin <me@kevin.jp>
2020-07-11 22:21:16 +0900

    add generator for rollup files

---
+++ rollup.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+erika_path="dotfiles/erika/.ssh/authorized_keys"
+primary_path="dotfiles/primary/.ssh/authorized_keys"
+
+files=$(ls rsa)
+
+echo -n "" > $erika_path
+echo -n "" > $primary_path
+
+for file in $files; do
+  echo "#$file" >> $erika_path
+  cat rsa/$file >> $erika_path
+done
+
+for file in $files ; do
+  if [ "$file" = "umaru" ] ; then
+    continue
+  fi
+  echo "#$file" >> $primary_path
+  cat rsa/$file >> $primary_path
+done
+
+jq_input=""
+for file in $files ; do
+  if [ "$file" = "umaru" ] ; then
+    continue
+  fi
+
+  jq_input+=$(echo "$file|$(cat rsa/$file)\n")
+done
+
+echo -en $jq_input | jq -R 'split("|") | {id: .[0], key: .[1]}' > ssh.json
